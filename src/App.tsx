003efdc0ab193096fd0e0c738aa56ba0a0e6474a
Arthur Cabral <63428806+CabralArthur@users.noreply.github.com>
2021-09-14 23:53:00 -0300

    Creating Main App + Adding Styled Components

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+const Title = styled.h1`
+  color: #F60;
+`
+
+export function App() {
+  return (
+    <div>
+      <Title>Hello World</Title>
+    </div>
+  );
+}
