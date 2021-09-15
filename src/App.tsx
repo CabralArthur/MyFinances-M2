a84562856c5cfaea5751f0765f37ff210e1e79ec
Arthur Cabral <63428806+CabralArthur@users.noreply.github.com>
2021-09-15 20:35:56 -0300

    Adding Dashboard to App

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import { Header } from "./components/Header";
 import { GlobalStyle } from "./styles/global";
+import { Dashboard } from "./components/Dashboard";
 
 
 export function App() {
   return (
     <div>
       <Header/>
+      <Dashboard/>
       <GlobalStyle/>
     </div>
   );
