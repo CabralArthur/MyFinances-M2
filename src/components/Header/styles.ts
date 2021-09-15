665c8eda3a27f265ba10d8766d2d35b2beebe91c
Arthur Cabral <63428806+CabralArthur@users.noreply.github.com>
2021-09-15 09:22:42 -0300

    Header Style

---
+++ src/components/Header/styles.ts
@@ -0,0 +1,34 @@
+import styled from "styled-components";
+
+export const Container = styled.header`
+    background: var(--dark);
+`
+
+export const Content = styled.div`
+    max-width: 1120px;
+    margin: 0 auto;
+
+    padding: 2rem 1rem 12rem;
+
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+
+    button{
+        font-size: 1rem;
+        color: var(--shape);
+
+        background-color: var(--orange);
+        height:3rem;
+        padding: 0 2rem;
+        
+        border: 0;
+        border-radius: 0.25rem;
+
+        transition: filter .2s;
+
+        &:hover{
+            filter: brightness(0.8);
+        }
+    }
+`
