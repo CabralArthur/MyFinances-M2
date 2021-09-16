42cd0bcee8da5be278bbb4a91f7afc7df3acb0dd
Arthur Cabral <63428806+CabralArthur@users.noreply.github.com>
2021-09-16 13:30:03 -0300

    Adding MirageJS, creating a fake API and fetching with Axios

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Server } from 'miragejs';
 import { App } from './App';
+
+new Server({
+  routes() {
+    this.namespace = '/api';
+
+    this.get('/transactions', () =>{
+      return [{
+        id: 1,
+        title: 'Transaction 1',
+        amount: 400,
+        type: 'deposit',
+        category: 'Food'
+      }]
+    })
+  }
+})
+
 
 ReactDOM.render(
   <React.StrictMode>
